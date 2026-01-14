1c7302d2bcfaac467ce016a74cd1755d8ea79efa
jreue <jreue@users.noreply.github.com>
2026-01-13 20:31:44 -0600

    Removed UART config

--- src/hardware_config.h
+++ src/hardware_config.h
@@ -1,13 +1,6 @@
 #pragma once
 
 // ====================
-// UART Configuration
-// ====================
-#define UART_BAUD_RATE 9600
-#define UART_RX_PIN 19
-#define UART_TX_PIN 21
-
-// ====================
 // Scan Button
 // ====================
 #define SCAN_DEVICE_PIN 32
