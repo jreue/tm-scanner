b9c6b6a8ea788ffa3688970e4d6ce7f3562c0622
jreue <jreue@users.noreply.github.com>
2026-01-14 16:24:12 -0600

    Refactor EspNowHelper to use shared message structs
    Replaces local message type definitions in EspNowHelper.h with shared structs from the tm-shared library. Adds tm-shared as a dependency in platformio.ini to centralize message structure definitions and improve code maintainability.

--- src/EspNowHelper.h
+++ src/EspNowHelper.h
@@ -1,33 +1,9 @@
 #pragma once
 
 #include <Arduino.h>
+#include <MessageStructs.h>
 #include <WiFi.h>
 #include <esp_now.h>
-
-// Message types
-#define MSG_TYPE_CONNECT 0
-#define MSG_TYPE_STATUS 1
-#define MSG_TYPE_DISCONNECT 2
-#define MSG_TYPE_DATE_UPDATE 3
-#define MSG_TYPE_SCANNER_CONNECTED 4
-
-// Common header for all messages
-struct EspNowHeader {
-    uint8_t id;
-    uint8_t messageType;
-};
-
-typedef struct ScannerMessage {
-    uint8_t id;
-    uint8_t messageType;  // MSG_TYPE_CONNECT, MSG_TYPE_STATUS, MSG_TYPE_DISCONNECT
-} ScannerMessage;
-
-// Device message (connection/calibration status)
-struct DeviceMessage {
-    uint8_t id;
-    uint8_t messageType;
-    bool isCalibrated;
-};
 
 class EspNowHelper {
   public:
